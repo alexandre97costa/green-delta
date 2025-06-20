c9224ca26f19347f8a1478697f2e0da403728917
Alexandre Costa <alexandre97costa@gmail.com>
2025-06-21 00:42:54 +0100

    cont. layout + locale + cores

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,14 @@
 	import { invalidate } from '$app/navigation';
 	import { page } from '$app/state';
 	import { onMount } from 'svelte';
+	import { m } from '$lib/paraglide/messages.js';
+	import { setLocale } from '$lib/paraglide/runtime';
 
 	import { Navigation } from '@skeletonlabs/skeleton-svelte';
 	import { AppBar } from '@skeletonlabs/skeleton-svelte';
 	import { Avatar } from '@skeletonlabs/skeleton-svelte';
 	import LucideIcon from '../components/LucideIcon.svelte';
-	import HeaderImg from '$lib/favicon.png'
+	import HeaderImg from '$lib/favicon.png';
 
 	let { children, data } = $props();
 
@@ -16,9 +18,9 @@
 	$effect: ({ supabase, session } = data);
 
 	const links = [
-		{ label: 'Leaderboard', href: 'files', isPrimary: false, icon: 'Crown'},
-		{ label: 'Novo Tempo', href: 'images', isPrimary: true, icon: 'ClockPlus' },
-		{ label: 'Torneios', href: 'videos', isPrimary: false, icon: 'Trophy' },
+		{ label: m.nav_leaderboard(), href: '/leaderboard', icon: 'Crown' },
+		{ label: m.nav_tournaments(), href: '/makes', icon: 'Trophy' },
+		{ label: m.nav_newtime(), href: '/new-time', icon: 'ClockPlus' }
 	];
 
 	onMount(() => {
@@ -36,25 +38,36 @@
 </svelte:head>
 
 <div class=" flex flex-col h-full w-full">
-	<header class="bg-black px-6 py-3 w-full flex gap-3 items-start">
-		<img src={HeaderImg} alt="Header icon" class="max-h-[40px]">
-		<h1 class="brand-font font-bold text-2xl">Green Delta</h1>
+	<header class="bg-black px-6 py-3 w-full flex items-center justify-between">
+		<!-- Left Header -->
+		<div class="flex gap-3 items-center">
+			<img src={HeaderImg} alt="Header icon" class="max-h-[40px]" />
+			<h1 class="brand-font font-bold text-2xl">Green Delta</h1>
+		</div>
+
+		<!-- Right Header -->
+		<nav class="btn-group preset-outlined-surface-200-800 p-2 ">
+			<button type="button" class="btn btn-sm preset-filled" onclick={() => setLocale('en')}>EN</button>
+			<button type="button" class="btn btn-sm hover:preset-tonal" onclick={() => setLocale('pt-pt')}>PT</button>
+		</nav>
 	</header>
 
 	<!-- Content -->
-	<div class="px-6 md:px-8 py-6 h-full">
+	<div class="px-6 md:px-8 py-6 h-full w-full">
 		{@render children()}
 	</div>
 
-	<div class="sticky bg-i bottom-0 w-full flex items-center justify-center gap-5 md:pb-5">
-		<Navigation.Bar width="rounded-lg w-full md:w-1/2 lg:w-1/3" background="bg-black">
-			{#each links as { label, href, isPrimary, icon, icon2 }, i}
+	<div class="sticky bg-i bottom-0 w-full flex items-center justify-center md:pb-5">
+		<Navigation.Bar classes="rounded-lg bg-black h-[50px]" width="w-full md:w-1/2 lg:w-1/3">
+			{#each links as { label, href, icon, icon2 }, i}
 				<Navigation.Tile
 					{label}
 					{href}
 					selected={page.url.pathname === href}
-					hover="hover:bg-primary-500"
-					background={isPrimary ? 'bg-tertiary-500' : ''}
+					height={'mh-20px'}
+					hover={'hover:border-1 hover:border-primary-500 '}
+					classes={'border-1 border-neutral-950 ' +
+						(page.url.pathname === href ? 'bg-primary-500 text-neutral-950' : 'text-neutral-50')}
 				>
 					<LucideIcon name={icon} />
 				</Navigation.Tile>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -14,40 +14,40 @@
 </script>
 
 <svelte:head>
-	<title>Login</title>
+	<title>Login | Green Delta</title>
 </svelte:head>
+<div class="w-full h-full flex justify-center items-center">
+	<form class="w-xl flex flex-col gap-3" method="POST" use:enhance={handleSubmit}>
 
-<form class=" flex flex-col h-full" method="POST" use:enhance={handleSubmit}>
-	<h1 class="header">Supabase + SvelteKit</h1>
-	
-	{#if form?.message !== undefined}
-		<div class="success {form?.success ? '' : 'fail'}">
-			{form?.message}
-		</div>
-	{/if}
+		{#if form?.message !== undefined}
+			<div class="success {form?.success ? '' : 'fail'}">
+				{form?.message}
+			</div>
+		{/if}
 
 		<label class="label" for="email">
 			<span class="label-text">Email address:</span>
 			<input
 				id="email"
 				name="email"
-				class="inputField"
+				class="input"
 				type="email"
 				placeholder="Your email"
 				value={form?.email ?? ''}
 			/>
 		</label>
-		<button type="button" class="btn preset-filled-primary-500">
+
+		<button type="button" class="btn btn-lg gradient-one">
 			{loading ? 'Loading' : 'Send magic link'}
 		</button>
 
-
-	{#if form?.errors?.email}
-		<span class="flex items-center text-sm error">
-			{form?.errors?.email}
-		</span>
-	{/if}
-</form>
+		{#if form?.errors?.email}
+			<span class="flex items-center text-sm error">
+				{form?.errors?.email}
+			</span>
+		{/if}
+	</form>
+</div>
 
 <style lang="postcss">
 	@reference "tailwindcss";
