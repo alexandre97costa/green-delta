16799b4d2405b24a422b360dc0fc859870fd5931
Alexandre Costa <alexandre97costa@gmail.com>
2025-06-20 22:14:18 +0100

    icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,21 +7,7 @@
 	import { Navigation } from '@skeletonlabs/skeleton-svelte';
 	import { AppBar } from '@skeletonlabs/skeleton-svelte';
 	import { Avatar } from '@skeletonlabs/skeleton-svelte';
-	// Icons
-	import IconFolder from '@lucide/svelte/icons/folder';
-	import IconImage from '@lucide/svelte/icons/image';
-	import IconMusic from '@lucide/svelte/icons/music';
-	import IconVideo from '@lucide/svelte/icons/video';
-	import IconBox from '@lucide/svelte/icons/box';
-	import ArrowLeft from '@lucide/svelte/icons/arrow-left';
-	import Paperclip from '@lucide/svelte/icons/paperclip';
-	import Calendar from '@lucide/svelte/icons/calendar';
-	import CircleUser from '@lucide/svelte/icons/circle-user';
-	import Menu from '@lucide/svelte/icons/menu';
-	import ClockPlus from '@lucide/svelte/icons/clock-plus';
-
-	import { Icon } from '@lucide/svelte';
-
+	import LucideIcon from '../components/LucideIcon.svelte';
 	import HeaderImg from '$lib/favicon.png'
 
 	let { children, data } = $props();
@@ -29,13 +15,10 @@
 	let { supabase, session } = data;
 	$effect: ({ supabase, session } = data);
 
-	const a = [IconImage, IconMusic, IconBox, CircleUser];
-
 	const links = [
-		{ label: 'Leaderboard', href: '#/files', isPrimary: false, icon: 'box' },
-		{ label: 'Torneios', href: '#/videos', isPrimary: false, icon: 'box' },
-		{ label: 'Pistas', href: '#/music', isPrimary: false, icon: 'box' },
-		{ label: 'Novo Tempo', href: '#/images', isPrimary: true, icon: 'calendar' }
+		{ label: 'Leaderboard', href: 'files', isPrimary: false, icon: 'Crown'},
+		{ label: 'Novo Tempo', href: 'images', isPrimary: true, icon: 'ClockPlus' },
+		{ label: 'Torneios', href: 'videos', isPrimary: false, icon: 'Trophy' },
 	];
 
 	onMount(() => {
@@ -53,19 +36,19 @@
 </svelte:head>
 
 <div class=" flex flex-col h-full w-full">
-	<header class="bg-black px-6 py-2 w-full flex gap-3 items-center">
-		<img src={HeaderImg} alt="Header icon" class="max-h-[60px]">
-		<h1 class="brand-font font-bold text-3xl">Green Delta</h1>
+	<header class="bg-black px-6 py-3 w-full flex gap-3 items-start">
+		<img src={HeaderImg} alt="Header icon" class="max-h-[40px]">
+		<h1 class="brand-font font-bold text-2xl">Green Delta</h1>
 	</header>
 
 	<!-- Content -->
-	<div class="px-20 py-5 h-full">
+	<div class="px-6 md:px-8 py-6 h-full">
 		{@render children()}
 	</div>
 
-	<div class="sticky bg-i bottom-0 w-full flex items-center justify-center gap-5 p-5">
-		<Navigation.Bar width="w-full md:w-1/2 lg:w-1/3" background="bg-black">
-			{#each links as { label, href, isPrimary, icon }, i}
+	<div class="sticky bg-i bottom-0 w-full flex items-center justify-center gap-5 md:pb-5">
+		<Navigation.Bar width="rounded-lg w-full md:w-1/2 lg:w-1/3" background="bg-black">
+			{#each links as { label, href, isPrimary, icon, icon2 }, i}
 				<Navigation.Tile
 					{label}
 					{href}
@@ -73,11 +56,7 @@
 					hover="hover:bg-primary-500"
 					background={isPrimary ? 'bg-tertiary-500' : ''}
 				>
-					{#if isPrimary}
-						<ClockPlus />
-					{:else}
-						<IconFolder />
-					{/if}
+					<LucideIcon name={icon} />
 				</Navigation.Tile>
 			{/each}
 		</Navigation.Bar>
