33bb55b24401028b58761521d29056ca2db7b3e3
Alexandre Costa <alexandre97costa@gmail.com>
2025-06-26 22:44:56 +0100

    query!

--- src/routes/leaderboard/+page.server.js
+++ src/routes/leaderboard/+page.server.js
@@ -1,8 +1,12 @@
 import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
-	const { data } = await supabase.from('car').select();
+	const { data } = await supabase.from("car").select(`
+		*,
+		car_make(*),
+		car_category(*)
+	`);
 	return {
-		car: data ?? []
+		cars: data ?? []
 	};
 }
--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -4,14 +4,17 @@
 	import { setLocale } from '$lib/paraglide/runtime';
 
 	let { data } = $props();
+
+	$effect(() => {
+		console.log(data)
+	});
 </script>
 
 <div>
 	<div>{m.nav_leaderboard()}</div>
-
 	<ul>
-		{#each data.car as car}
-			<li>{car.name}</li>
+		{#each data.cars as car}
+			<li>{car.car_make.name} {car.model} {car.car_category.name} {car.year}</li>
 		{/each}
 	</ul>
 </div>
